14c42d4ffcd4bab6d6babef684624a3b92074620
LeeKwonWoo <kenu12@naver.com>
2023-06-12 21:33:58 +0900

    메인 홈페이지 수정

--- BookMarket/src/main/java/com/springmvc/repository/BookRepositoryImpl.java
+++ BookMarket/src/main/java/com/springmvc/repository/BookRepositoryImpl.java
@@ -161,10 +161,10 @@ public class BookRepositoryImpl implements BookRepository {
 	public void setUpdateBook(Book book) {
 		if (book.getFileName() != null) {
 			String SQL = "UPDATE book SET b_name = ?, b_unitPrice = ?, b_author = ?, b_description = ?, b_publisher = ?, b_category = ?, b_unitsInStock = ?, b_releaseDate = ?, b_condition = ?, b_fileName = ? where b_bookId = ?";
-			template.update(SQL, book.getBookId(), book.getName(), book.getUnitPrice(), book.getAuthor(), book.getDescription(), book.getPublisher(), book.getCategory(), book.getUnitsInStock(), book.getReleaseDate(), book.getCondition(), book.getFileName(), book.getBookId());
+			template.update(SQL, book.getName(), book.getUnitPrice(), book.getAuthor(), book.getDescription(), book.getPublisher(), book.getCategory(), book.getUnitsInStock(), book.getReleaseDate(), book.getCondition(), book.getFileName(), book.getBookId());
 		} else if (book.getFileName() == null) {
 			String SQL = "UPDATE book SET b_name = ?, b_unitPrice = ?, b_author = ?, b_description = ?, b_publisher = ?, b_category = ?, b_unitsInStock = ?, b_releaseDate = ?, b_condition = ? where b_bookId = ?";
-			template.update(SQL, book.getBookId(), book.getName(), book.getUnitPrice(), book.getAuthor(), book.getDescription(), book.getPublisher(), book.getCategory(), book.getUnitsInStock(), book.getReleaseDate(), book.getCondition(), book.getBookId());
+			template.update(SQL, book.getName(), book.getUnitPrice(), book.getAuthor(), book.getDescription(), book.getPublisher(), book.getCategory(), book.getUnitsInStock(), book.getReleaseDate(), book.getCondition(), book.getBookId());
 		}
 	}
 	
